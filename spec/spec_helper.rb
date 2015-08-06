134255962f2be3d81ed8c0aad781eeb65de62cc6
Camilo Castro <camilo@jumpitt.com>
2015-08-06 16:14:25 -0300

    enable deprecated test syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $LOAD_PATH << "./lib"
 require 'guard/xctool-test'
+
+# Enable old and new syntax
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
